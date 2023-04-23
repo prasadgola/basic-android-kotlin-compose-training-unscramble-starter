18bbc468d49404b0be4b2c3e74d6a1901fefb861
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-04-22 21:06:29 -0500

    Add stateflow

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.android.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 data class GameUiState(
     val currentScrambledWord: String = ""
-)
+) {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+}
