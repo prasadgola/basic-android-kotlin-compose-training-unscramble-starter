8e39ef8f9350355a7e0fbb2ff61b5b90f754e29d
Basavaprasad Mallikarjun Gola <basavaprasad.m.gola@gmail.com>
2023-04-23 11:44:29 -0500

    Display random scrambled word with undefined variables

--- basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
+++ basic-android-kotlin-compose-training-unscramble-starter/app/src/main/java/com/example/android/unscramble/ui/GameViewModel.kt
@@ -4,10 +4,48 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import com.example.android.unscramble.data.allWords
 
 data class GameUiState(
     val currentScrambledWord: String = ""
 ) {
     private val _uiState = MutableStateFlow(GameUiState())
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+}
+
+
+
+
+private fun pickRandomWordAndShuffle(): String {
+    // Continue picking up a new random word until you get one that hasn't been used before
+    currentWord = allWords.random()
+    if (usedWords.contains(currentWord)) {
+        return pickRandomWordAndShuffle()
+    } else {
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+}
+
+// Set of words used in the game
+private var usedWords: MutableSet<String> = mutableSetOf()
+
+private fun shuffleCurrentWord(word: String): String {
+    val tempWord = word.toCharArray()
+    // Scramble the word
+    tempWord.shuffle()
+    while (String(tempWord).equals(word)) {
+        tempWord.shuffle()
+    }
+    return String(tempWord)
+}
+
+fun resetGame() {
+    usedWords.clear()
+    _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+}
+
+
+init {
+    resetGame()
 }
